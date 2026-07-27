503759855b6f61e3627fc5e8b004bab0180d87c9
Newark <brunowmanha@gmail.com>
2026-07-27 15:53:32 -0300

    criados os 3 microservicos, clientes, produtos e servicos e tambem criado o yml de servicos

---
+++ produtos/produtos/src/test/java/com/microservicos/icompras/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicos.icompras.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
