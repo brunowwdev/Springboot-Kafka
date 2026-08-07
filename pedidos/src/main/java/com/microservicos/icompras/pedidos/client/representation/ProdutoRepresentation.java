eb3772009714d6054460fbf6871b1d67a364314c
Newark <brunowmanha@gmail.com>
2026-08-07 09:25:10 -0300

    implementado servico de pedidos

---
+++ pedidos/src/main/java/com/microservicos/icompras/pedidos/client/representation/ProdutoRepresentation.java
@@ -0,0 +1,7 @@
+package com.microservicos.icompras.pedidos.client.representation;
+
+import java.math.BigDecimal;
+
+public record ProdutoRepresentation(Long codigo, String nome, BigDecimal valorUnitario) {
+
+}
--- pedidos/src/main/java/com/microservicos/icompras/pedidos/validator/PedidoValidator.java
+++ pedidos/src/main/java/com/microservicos/icompras/pedidos/validator/PedidoValidator.java
@@ -1,12 +1,26 @@
 package com.microservicos.icompras.pedidos.validator;
 
+import com.microservicos.icompras.pedidos.client.ProdutosClient;
+import com.microservicos.icompras.pedidos.client.representation.ProdutoRepresentation;
 import com.microservicos.icompras.pedidos.model.Pedido;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+
 @Component
+@RequiredArgsConstructor
 public class PedidoValidator {
 
-    public void validar(Pedido pedido){
+    private final ProdutosClient produtosClient;
 
+    public void validar(Pedido pedido){
+        List<Long> codigosProdutos = pedido.getItens().stream().map(i -> i.getCodigoProduto()).toList();
+        codigosProdutos.forEach(codigoProduto -> {
+            ResponseEntity<ProdutoRepresentation> response = produtosClient.obterDados(codigoProduto);
+            ProdutoRepresentation produto = response.getBody();
+        });
     }
 }
