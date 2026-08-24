d57c6d1ce08f26a228db936093627b1b9fada5ea
Newark <brunowmanha@gmail.com>
2026-08-24 13:10:45 -0300

    metodo acessar a url dos arquivos no minio

--- faturamento/src/main/java/io/github/curso/icompras/faturamento/api/BucketController.java
+++ faturamento/src/main/java/io/github/curso/icompras/faturamento/api/BucketController.java
@@ -3,12 +3,10 @@ package io.github.curso.icompras.faturamento.api;
 import io.github.curso.icompras.faturamento.bucket.BucketFile;
 import io.github.curso.icompras.faturamento.bucket.BucketService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -31,6 +29,16 @@ public class BucketController {
 
         }catch (Exception e){
             return ResponseEntity.status(500).body("Erro ao enviar arquivo" + e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getUrl(@RequestParam String filename){
+        try{
+            String url = service.getUrl(filename);
+            return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).body(url);
+        }catch (Exception e){
+            return ResponseEntity.status(500).body("Erro ao obter URL do arquivo" + e.getMessage());
         }
     }
 }
