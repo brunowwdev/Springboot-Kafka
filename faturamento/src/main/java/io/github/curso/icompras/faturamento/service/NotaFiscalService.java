f4ec2191aed9306df6082c73bf345d6bc17c9e35
Newark <brunowmanha@gmail.com>
2026-09-02 08:48:49 -0300

    criado a nota fiscal no jasper reports e criado o NotaFiscalService para gerar o pdf

---
+++ faturamento/src/main/java/io/github/curso/icompras/faturamento/service/NotaFiscalService.java
@@ -0,0 +1,48 @@
+package io.github.curso.icompras.faturamento.service;
+
+import io.github.curso.icompras.faturamento.model.Pedido;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class NotaFiscalService {
+
+    @Value("${classpath:reports/nota-fiscal.jrxml}")
+    private Resource notaFiscal;
+    @Value("${classpath:reports/logo.png}")
+    private Resource logo;
+
+    public byte[] getNota(Pedido pedido) {
+        try (InputStream is = notaFiscal.getInputStream()){
+
+            Map<String,Object> params = new HashMap<>();
+            params.put("NOME", pedido.cliente().nome());
+            params.put("CPF", pedido.cliente().cpf());
+            params.put("LOGRADOURO", pedido.cliente().logradouro());
+            params.put("NUMERO", pedido.cliente().numero());
+            params.put("BAIRRO", pedido.cliente().bairro());
+            params.put("EMAIL", pedido.cliente().email());
+            params.put("TELEFONE", pedido.cliente().telefone());
+
+            params.put("DATA_PEDIDO", pedido.data());
+            params.put("TOTAL_PEDIDO", pedido.total());
+
+            var datasource = new JRBeanCollectionDataSource(pedido.itens());
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(is);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, datasource); // passou relatorio, os parametros e os campos
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
