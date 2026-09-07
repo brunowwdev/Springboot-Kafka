d9f01e3302afc86dabd12144302c8411eaf012ea
Newark <brunowmanha@gmail.com>
2026-09-07 10:05:49 -0300

    atualizado o status do pedido no banco de dados e tambem no json e implementado metodo para retornar o status e url da nota fiscal em detalhes do pedido

--- pedidos/src/main/java/com/microservicos/icompras/pedidos/service/AtualizacaoStatusPedidoService.java
+++ pedidos/src/main/java/com/microservicos/icompras/pedidos/service/AtualizacaoStatusPedidoService.java
@@ -1,10 +1,10 @@
 package com.microservicos.icompras.pedidos.service;
 
 import com.microservicos.icompras.pedidos.Repository.PedidoRepository;
-import com.microservicos.icompras.pedidos.model.Pedido;
 import com.microservicos.icompras.pedidos.model.enums.StatusPedido;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,8 +12,13 @@ public class AtualizacaoStatusPedidoService {
 
     private final PedidoRepository pedidoRepository;
 
+    @Transactional
     public void atualizarStatus(Long codigo, StatusPedido status, String urlNotaFiscal, String codigoRastreio) {
 
-
+        pedidoRepository.findById(codigo).ifPresent( pedido -> {
+            pedido.setStatus(status);
+            pedido.setUrlNotaFiscal(urlNotaFiscal);
+            pedido.setCodigoRastreio(codigoRastreio);
+        } );
     }
 }
